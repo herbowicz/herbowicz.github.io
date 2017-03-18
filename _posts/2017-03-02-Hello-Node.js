6a38ef272eaec68216f2605bb2b33359e3deb699
Herbowicz <herbowicz@users.noreply.github.com>
2017-03-18 21:35:09 +0100

    short

--- _posts/2017-03-02-Hello-Node.js
+++ _posts/2017-03-02-Hello-Node.js
@@ -8,21 +8,11 @@ lang: en-EN
 To start with and check localhost:8080...
 
 {% highlight javascript %}
-var http = require('http');
 
+var http = require('http');
 http.createServer(function(request, response) {
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
-  var body = [];
-  request.on('error', function(err) {
-    console.error(err);
-  }).on('data', function(chunk) {
-    body.push(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-  });
-response.end('<html><body><h1>Hello from Node.js!</h1></body></html>');
-}).listen(8080);
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello from Node.js!');
+}).listen(1234);
 
 {% endhighlight %}
